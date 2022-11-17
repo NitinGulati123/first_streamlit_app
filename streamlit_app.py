96671ae49579a42578ca3aebf7e90f99f957f975
NitinGulati123 <118032238+NitinGulati123@users.noreply.github.com>
2022-11-17 10:57:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-streamlit.text("Hello from Snowflake:") ---Additional hello
+streamlit.text("Hello from Snowflake:") ##---Additional hello
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
